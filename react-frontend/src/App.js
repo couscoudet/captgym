3359a6c57e427b5cb37cbc260155c177057ba86b
yannick SOULTANE <ysoultane@gmail.com>
2022-09-18 09:44:25 +0200

    add bootstrap css and test example

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import MyTable from './MyTable';
+import Example from './Example';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <MyTable />
+      <Example />
     </div>
   );
 }
