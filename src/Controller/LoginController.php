885791dd46024e28b57e4bbaf1e988cd84795f43
yannick SOULTANE <ysoultane@gmail.com>
2022-09-29 21:32:48 +0200

    configure jwt token

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LoginController extends AbstractController
 {
-    #[Route('/', name: 'app_login')]
+    #[Route('/test', name: 'app_login2')]
     public function index(): Response
     {
         return $this->render('login/index.html.twig', [
